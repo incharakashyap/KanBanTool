8b438e10279429eb4fe62a0e1282c1074701f485
incharakashyap <incharakashyap1@gmail.com>
2023-05-12 11:45:31 +0530

    first commit

---
+++ KanbanToolB/src/main/java/com/example/KanbanToolB/KanbanToolBApplication.java
@@ -0,0 +1,13 @@
+package com.example.KanbanToolB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanToolBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanToolBApplication.class, args);
+	}
+
+}
---
+++ KanbanToolB/src/test/java/com/example/KanbanToolB/KanbanToolBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KanbanToolB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanToolBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
